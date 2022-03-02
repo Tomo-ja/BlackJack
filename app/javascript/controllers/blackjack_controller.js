8b8e6377c3f837ea9a5f2c47a41aecf623edda33
Tomo-ja <tomonao.hj@gmail.com>
2022-03-01 20:55:15 -0800

    tired

--- app/javascript/controllers/blackjack_controller.js
+++ app/javascript/controllers/blackjack_controller.js
@@ -1,5 +1,49 @@
 import { Controller } from "@hotwired/stimulus"
-import { noMoreTen, hasAce, checkOver21, whoIsWin, checkOver21ForHidden } from "./functions"
+// import { noMoreTen, hasAce, checkOver21, whoIsWin, checkOver21ForHidden } from "./functions"
+
+
+let noMoreTen = (num) => {
+	if (num > 10 ){
+		return 10
+	} else {
+		return num
+	}
+}
+let hasAce = (num) => {
+	if (num == 1){
+		return true
+	}
+}
+let showResult = (field, result) => {
+	field.classList.remove('hidden')
+	field.classList.add('flex')
+	field.querySelector('h2').innerText = `${result}`
+}
+let checkOver21 = (num, field, result) => {
+	if( num > 21 ){
+		showResult(field, result)
+		console.log(num)
+	}
+}
+
+let checkOver21ForHidden = (element, field) => {
+	if (parseInt(element.innerText) > 21) {
+		field.classList.add('hidden')
+	}
+}
+
+let whoIsWin = (playerTotal, dealerTotal, field)=>{
+	if(playerTotal == dealerTotal){
+		showResult(field, "It's Tie.")
+		console.log("tie")
+	}else if (playerTotal > dealerTotal){
+		showResult(field, "You win")
+		console.log("win")
+	}else{
+		showResult(field, "You Lose")
+		console.log("lose")
+	}
+}
 
 export default class extends Controller{
 	connect(){
